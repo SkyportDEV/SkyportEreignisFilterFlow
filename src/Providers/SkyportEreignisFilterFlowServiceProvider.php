4733299efee2834fcbb93576644302e3b5268190
SkyportDEV <a.schieder@skyport-group.de>
2026-02-12 21:11:50 +0100

    Update SkyportEreignisFilterFlowServiceProvider.php

--- src/Providers/SkyportEreignisFilterFlowServiceProvider.php
+++ src/Providers/SkyportEreignisFilterFlowServiceProvider.php
@@ -10,8 +10,6 @@ class SkyportEreignisFilterFlowServiceProvider extends ServiceProvider
 {
     public function boot(FilterDefinitionContainer $filterDefinitionContainer): void
     {
-        // Wichtig: In vielen Plenty-Versionen ist das die stabilste Form:
-        // => Klasse registrieren, nicht pluginApp() Instanz
         $filterDefinitionContainer->register(SkyportIdListFilter::class);
     }
 }
