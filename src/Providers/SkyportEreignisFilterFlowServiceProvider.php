c152fe36de5a3cd7d5f9ec6fe394ff99f855f3c6
SkyportDEV <a.schieder@skyport-group.de>
2026-02-13 09:00:06 +0100

    Update SkyportEreignisFilterFlowServiceProvider.php

--- src/Providers/SkyportEreignisFilterFlowServiceProvider.php
+++ src/Providers/SkyportEreignisFilterFlowServiceProvider.php
@@ -4,12 +4,12 @@ namespace SkyportEreignisFilterFlow\Providers;
 
 use Plenty\Plugin\ServiceProvider;
 use Plenty\Modules\Flow\Filters\Definitions\Containers\FilterDefinitionContainer;
-use SkyportEreignisFilterFlow\Flow\Filters\SkyportIdListFilter;
+use SkyportEreignisFilterFlow\Flow\Filters\SkyportAlwaysTrueFilter;
 
 class SkyportEreignisFilterFlowServiceProvider extends ServiceProvider
 {
-    public function boot(FilterDefinitionContainer $filterDefinitionContainer): void
+    public function boot(FilterDefinitionContainer $container): void
     {
-        $filterDefinitionContainer->register(pluginApp(SkyportIdListFilter::class));
+        $container->register(pluginApp(SkyportAlwaysTrueFilter::class));
     }
 }
