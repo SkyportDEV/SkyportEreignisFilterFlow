b0f09a5cbc1cdedf9c6cb7548c79c2fc30e9854a
SkyportDEV <a.schieder@skyport-group.de>
2026-08-27 20:46:44 +0200

    Update SkyportEreignisFilterFlowServiceProvider.php

--- src/Providers/SkyportEreignisFilterFlowServiceProvider.php
+++ src/Providers/SkyportEreignisFilterFlowServiceProvider.php
@@ -4,18 +4,28 @@ namespace SkyportEreignisFilterFlow\Providers;
 
 use Plenty\Plugin\ServiceProvider;
 use Plenty\Modules\Flow\Services\PluginFlowRegistrationService;
-use SkyportEreignisFilterFlow\Flow\Filters\SkyportIdListFilter;
+use SkyportEreignisFilterFlow\Flow\Filters\SkyportContactIdFilter;
+use SkyportEreignisFilterFlow\Flow\Filters\SkyportBillingAddressIdFilter;
+use SkyportEreignisFilterFlow\Flow\Filters\SkyportDeliveryAddressIdFilter;
 
 class SkyportEreignisFilterFlowServiceProvider extends ServiceProvider
 {
     public function boot(): void
     {
-        $pluginFlowRegistrationService = pluginApp(
+        $registrationService = pluginApp(
             PluginFlowRegistrationService::class
         );
 
-        $pluginFlowRegistrationService->registerFilter(
-            pluginApp(SkyportIdListFilter::class)
+        $registrationService->registerFilter(
+            pluginApp(SkyportContactIdFilter::class)
+        );
+
+        $registrationService->registerFilter(
+            pluginApp(SkyportBillingAddressIdFilter::class)
+        );
+
+        $registrationService->registerFilter(
+            pluginApp(SkyportDeliveryAddressIdFilter::class)
         );
     }
 }
